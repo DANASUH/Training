8b3670bd2802753be8b2bc8ddf0998f9cb44d62f
DANA <ix01da01@gmail.com>
2026-07-26 13:50:34 +0300

    Add react projects

--- exercise-01/script.js
+++
@@ -1,5 +0,0 @@
-const name = "Dana";
-let age = 23;
-console.log(`My name is ${name} and I am ${age}`);
-age = 22;
-console.log(`My name is ${name} and I am ${age}`);
--- exercise-05/script.js
+++
@@ -1,5 +0,0 @@
-const heading = document.querySelector("#main-heading");
-heading.textContent = "Hello World! JavaScript has taken control";
-heading.style.color = "royalblue";
-heading.classList.add("highlight");
-console.log("Heading successfully manipulated:", heading);
